1a0f79549b629bfab449c00ce92bc0da664054b7
hatim2022 <hatim.joubair@hotmail.com>
2018-12-31 03:49:16 +0100

    Angular nestjs
    users posts with angular and json

---
+++ clients/src/app/layout/users/user-main/user-main.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-main',
+  templateUrl: './user-main.component.html',
+  styleUrls: ['./user-main.component.scss']
+})
+export class UserMainComponent implements OnInit {
+varx:boolean = false;
+showadd() {
+  this.varx=true;
+}
+hideadd() {
+  this.varx=false;
+}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ clients/src/app/layout/users/users/users.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../user';
+import {UsersService} from '../users.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor(private userService: UsersService) {
+  }
+
+  ngOnInit() {
+    this.userService.load();
+    this.userService.users$.subscribe(value => {
+      console.log(value);
+      this.users = value;
+    });
+  }
+
+  delete(user: User) {
+    this.userService.delete(user.id).subscribe(value => {
+      this.userService.setUsers(this.users.filter(value1 => value1.id !== user.id));
+    }, error1 => {
+      console.log(error1);
+    });
+  }
+
+}
